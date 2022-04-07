7b7d6e7a5bfb1cfb5d93c6a4e65473a207be1487
WuZhichengmy <ljwzcyx@gmail.com>
2022-04-07 11:02:56 +0800

    更换鸟素材、背景

--- Assets/Scripts/Game/RoleControl.cs
+++ Assets/Scripts/Game/RoleControl.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class RoleControl : MonoBehaviour
 {
-    private float xMin=-9.9f;
-    private float xMax=9.9f;
-    private float yMin=-5.4f;
-    private float yMax=5.7f;
+    private float xMin=-14.9f;
+    private float xMax=14f;
+    private float yMin=-8.6f;
+    private float yMax=6.9f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +21,8 @@ public class RoleControl : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
         //position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.x = Mathf.Clamp(position.x + 3.0f * horizontal * Time.deltaTime,xMin,xMax);
-        position.y = Mathf.Clamp(position.y + 3.0f * vertical * Time.deltaTime, yMin, yMax);
+        position.x = Mathf.Clamp(position.x + 5.0f * horizontal * Time.deltaTime,xMin,xMax);
+        position.y = Mathf.Clamp(position.y + 5.0f * vertical * Time.deltaTime, yMin, yMax);
         //position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }

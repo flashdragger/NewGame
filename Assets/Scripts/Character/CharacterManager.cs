6171f2c0da468a8d36633ea49f38481bd08df5d6
jason-chen66 <1005628190@qq.com>
2022-05-02 10:15:47 +0800

    1

--- Assets/Scripts/Character/CharacterManager.cs
+++ Assets/Scripts/Character/CharacterManager.cs
@@ -18,11 +18,7 @@ public class CharacterManager : MonoBehaviour
  
     private void Start() {
         foreach (var i in PrefabList)
-<<<<<<< HEAD
-      _characters.Add(Instantiate<GameObject>(i, transform));
-=======
             _characters.Add(Instantiate<GameObject>(i, transform));
->>>>>>> 2ce4bce5b1eeed8223bf8b2cb858de86bfcf796f
         ChangeCharacter(DefaultCharacter);
         _coldTimer = 0f;
     }

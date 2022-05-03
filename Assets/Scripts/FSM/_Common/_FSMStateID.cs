7066ff7b319b8cbd65812352646f61ce44d8f63a
jason-chen66 <1005628190@qq.com>
2022-05-03 21:18:43 +0800

    asserts

---
+++ Assets/Scripts/FSM/_Common/_FSMStateID.cs
@@ -0,0 +1,15 @@
+namespace AI.FSM {
+
+    /// <summary>
+    /// 有限状态机状态枚举
+    /// </summary>
+    public enum _FSMStateID {
+        Default,
+        Idle,
+        Dash,
+        
+        emission
+
+    }
+
+}
